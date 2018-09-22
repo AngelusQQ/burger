efeb63298552c87016eabe809558810776a8c0a7
William <william.ma9215@gmail.com>
2018-09-22 13:32:22 -0400

    Save

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,8 +10,22 @@ router.get("/", function(req, res) {
   res.render("index");
 });
 
-router.get("/imageDevelopment", function(req, res) {
+router.post("/submit-order", function(req, res) {
+  console.log(req.body.order);
+  res.redirect("/");
+  // var node = document.createElement("<img>");
+  // node.setAttribute("src", )
+  //Double, Triple
+  //Burger, Beef, Chicken, Fish, Veggie,
+  //Lettuce, Tomato, Onions, Cheese, Pickles,
+});
+
+router.get("/title", function(req, res) {
   res.sendFile(path.join(__dirname, "../views/layouts/title.jpg"));
+});
+
+router.get("/submit-image", function(req, res){
+  res.sendFile(path.join(__dirname, "../views/layouts/submit.jpg"));
 });
 
 module.exports = router;
